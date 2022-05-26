a6131e68553e6687a7aa057fc8d6f22b8dd936a5
SpAnitha <103819801+SpAnitha@users.noreply.github.com>
2022-05-25 23:08:23 -0700

    Add files via upload

---
+++ Read.java
@@ -0,0 +1,34 @@
+package week5.day2Assignment2;
+import java.io.IOException;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+public class Read {
+	public static String[][] readData(String excelFileName) throws IOException 
+	{
+		XSSFWorkbook book = new XSSFWorkbook("./testData/" + excelFileName + "002.xlsx");
+		XSSFSheet sheet = book.getSheetAt(0);
+		int rowCount = sheet.getLastRowNum();
+		System.out.println("Row Count: " + rowCount);
+		XSSFRow headerRow = sheet.getRow(0);
+		short colCount = headerRow.getLastCellNum();
+		System.out.println("Column Count: " + colCount);
+		String[][] data = new String[rowCount][colCount];
+		for (int i = 1; i <= rowCount; i++) 
+		{
+			XSSFRow eachRow = sheet.getRow(i);
+			for (int j = 0; j < colCount; j++) 
+			{
+				XSSFCell eachCell = eachRow.getCell(j);
+				String value = eachCell.getStringCellValue();
+				data[i - 1][j] = value;
+			}
+			System.out.println();
+		}
+		return data;
+	}
+}
+
+
+
